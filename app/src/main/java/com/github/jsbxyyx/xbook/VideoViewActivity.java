3ad3c7d5321ba1751d033b4312df167f868d8894
jsbxyyx <jsbxyyx@163.com>
2024-07-09 14:28:46 +0800

    remove unused activity

--- app/src/main/java/com/github/jsbxyyx/xbook/VideoViewActivity.java
+++ app/src/main/java/com/github/jsbxyyx/xbook/VideoViewActivity.java
@@ -1,95 +1,95 @@
-package com.github.jsbxyyx.xbook;
-
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.github.jsbxyyx.xbook.common.LogUtil;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-/**
- * @author jsbxyyx
- */
-@Deprecated
-public class VideoViewActivity extends AppCompatActivity {
-
-    private WebView webView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_view);
-
-        Intent intent = getIntent();
-        String playUrl = intent.getStringExtra("playUrl");
-        webView = findViewById(R.id.wv_video_view);
-
-        WebSettings settings = webView.getSettings();
-        settings.setUserAgentString("Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36");
-        settings.setJavaScriptEnabled(true);
-        settings.setPluginState(WebSettings.PluginState.ON);
-        settings.setUseWideViewPort(true);
-        settings.setAllowFileAccess(true);
-        settings.setSupportZoom(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setUseWideViewPort(true);
-        settings.setDomStorageEnabled(true);
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
-            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-
-        webView.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                LogUtil.d(getClass().getSimpleName(), "url: %s", url);
-                view.loadUrl(url);
-                return true;
-            }
-
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                try {
-                    URL url = new URL(request.getUrl().toString());
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                LogUtil.e(getClass().getSimpleName(), request + "");
-                return super.shouldInterceptRequest(view, request);
-            }
-        });
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-            }
-        });
-
-        //webView.clearCache(true);
-        webView.loadUrl(playUrl);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (webView != null) {
-            webView.destroy();
-        }
-    }
-}
+//package com.github.jsbxyyx.xbook;
+//
+//import android.content.Intent;
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.webkit.WebResourceError;
+//import android.webkit.WebResourceRequest;
+//import android.webkit.WebResourceResponse;
+//import android.webkit.WebSettings;
+//import android.webkit.WebView;
+//import android.webkit.WebViewClient;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.github.jsbxyyx.xbook.common.LogUtil;
+//
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//
+///**
+// * @author jsbxyyx
+// */
+//@Deprecated
+//public class VideoViewActivity extends AppCompatActivity {
+//
+//    private WebView webView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_video_view);
+//
+//        Intent intent = getIntent();
+//        String playUrl = intent.getStringExtra("playUrl");
+//        webView = findViewById(R.id.wv_video_view);
+//
+//        WebSettings settings = webView.getSettings();
+//        settings.setUserAgentString("Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36");
+//        settings.setJavaScriptEnabled(true);
+//        settings.setPluginState(WebSettings.PluginState.ON);
+//        settings.setUseWideViewPort(true);
+//        settings.setAllowFileAccess(true);
+//        settings.setSupportZoom(true);
+//        settings.setLoadWithOverviewMode(true);
+//        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+//        settings.setUseWideViewPort(true);
+//        settings.setDomStorageEnabled(true);
+//
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+//            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+//        }
+//
+//        webView.setWebViewClient(new WebViewClient() {
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                LogUtil.d(getClass().getSimpleName(), "url: %s", url);
+//                view.loadUrl(url);
+//                return true;
+//            }
+//
+//            @Override
+//            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+//                try {
+//                    URL url = new URL(request.getUrl().toString());
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                }
+//                LogUtil.e(getClass().getSimpleName(), request + "");
+//                return super.shouldInterceptRequest(view, request);
+//            }
+//        });
+//
+//        webView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//            }
+//
+//            @Override
+//            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+//                super.onReceivedError(view, request, error);
+//            }
+//        });
+//
+//        //webView.clearCache(true);
+//        webView.loadUrl(playUrl);
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        if (webView != null) {
+//            webView.destroy();
+//        }
+//    }
+//}
